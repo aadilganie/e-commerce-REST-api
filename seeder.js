eb8a205eab582c59580a778de47e5208e1d097d0
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-13 20:53:08 -0700

    create dummy data for ShopItems

--- seeder.js
+++ seeder.js
@@ -3,14 +3,18 @@ const connectDB = require("./config/db");
 const fs = require("fs");
 
 const Shop = require("./model/Shop");
+const ShopItem = require("./model/ShopItem");
 
 connectDB();
 
 const shopData = JSON.parse(fs.readFileSync("./_data/shops.json", "utf8"));
-
+const shopItemData = JSON.parse(
+  fs.readFileSync("./_data/shopItems.json", "utf8")
+);
 const importData = async () => {
   try {
     await Shop.create(shopData);
+    await ShopItem.create(shopItemData);
     console.log("Data imported to database");
     process.exit();
   } catch (error) {
@@ -21,6 +25,7 @@ const importData = async () => {
 const destroyData = async () => {
   try {
     await Shop.deleteMany();
+    await ShopItem.deleteMany();
     console.log("Data destroyed");
     process.exit();
   } catch (error) {
