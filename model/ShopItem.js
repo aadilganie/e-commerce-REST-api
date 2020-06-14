d95c12c40ac8494bff4a9019f6645b002800fd9a
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-13 20:44:37 -0700

    add ShopItem model

---
+++ model/ShopItem.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ShopItemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required for each item"],
+    maxlength: [50, "Title must be no more than 50 characters"],
+  },
+  description: {
+    type: String,
+    maxlength: [500, "Description must be no more than 50 characters"],
+  },
+  category: {
+    type: String,
+    enum: ["clothing", "electronics", "home"],
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  qtySold: {
+    type: Number,
+    validate: function (v) {
+      return Number.isInteger(v);
+    },
+    message: "quantity must be of integer",
+    required: [true, "Quantity is required"],
+  },
+  shop: {
+    type: mongoose.Types.ObjectId,
+    ref: "Shop",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("ShopItem", ShopItemSchema);
