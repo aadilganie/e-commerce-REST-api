415da0e65bd6966256578f8bc1781887d30768e6
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-15 15:47:57 -0700

    add Review dummydata and setup import via seeder.js

--- seeder.js
+++ seeder.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const Shop = require("./model/Shop");
 const ShopItem = require("./model/ShopItem");
 const User = require("./model/User");
+const Review = require("./model/Review");
 
 connectDB();
 
@@ -13,11 +14,14 @@ const shopItemData = JSON.parse(
   fs.readFileSync("./_data/shopItems.json", "utf8")
 );
 const userData = JSON.parse(fs.readFileSync("./_data/User.json", "utf8"));
+const reviewData = JSON.parse(fs.readFileSync("./_data/Reviews.json", "utf8"));
+
 const importData = async () => {
   try {
     await Shop.create(shopData);
     await ShopItem.create(shopItemData);
     await User.create(userData);
+    await Review.create(reviewData);
     console.log("Data imported to database");
     process.exit();
   } catch (error) {
@@ -30,6 +34,7 @@ const destroyData = async () => {
     await Shop.deleteMany();
     await ShopItem.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data destroyed");
     process.exit();
   } catch (error) {
