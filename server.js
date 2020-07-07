cb9f42165c40fadc919787defdd574acada377e0
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-06 22:59:32 -0700

    Add catch all route

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.use(errorHandler);
 // http://localhost:5000/uploads/photo_5ee59cc1ac350f856d6d273f.jpeg
 app.use(express.static(path.join(__dirname, "public")));
 
+// Catch all route
+app.use("*", (req, res) => {
+  res.status(404).json({
+    error: "Not a valid route",
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
